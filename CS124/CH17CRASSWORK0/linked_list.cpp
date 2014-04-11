d5a9ae4ace9accfcc84f40e2ac5bc8c25057a1b4
Chris Miller <lordsauronthegreat@gmail.com>
2014-04-11 13:58:09 -0700

    feeling guilty about not dating it correctly
    not that it matters at all. sony will probably dmca my ass for using a for loop or something.

--- CS124/CH17CRASSWORK0/linked_list.cpp
+++ CS124/CH17CRASSWORK0/linked_list.cpp
@@ -1,7 +1,7 @@
 /* Chris Miller (imperator@pax-imperia.com), CS124
  * Copyright (C) 2014 Chris Miller.  Aw richts pitten by.
  * Academic endorsement.  This code is not licensed for commercial use.
- * 20140410, Chapter 17 Class Work 0
+ * 20140411, Chapter 17 Class Work 0
  */
 
 #include "linked_list.hpp"
